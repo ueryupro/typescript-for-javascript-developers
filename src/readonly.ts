efb3115a5767b2908415e28a0373e358028fb6f4
ueryu <nittymahi@gmail.com>
2021-07-04 21:39:31 +0900

    readonly

---
+++ src/readonly.ts
@@ -0,0 +1,9 @@
+export {};
+
+class VisaCard {
+  constructor(public readonly owner: string) {}
+}
+
+let myVisaCard = new VisaCard("ハムさん");
+console.log(myVisaCard.owner);
+// myVisaCard.owner = "ベーコン";
