eb1a506b1540c54b30de92e5cdf93ced14dc9ca0
ryuji.ueda <uedaryuji@uedaryuujinoMacBook-Pro.local>
2022-01-23 16:31:56 +0900

    feat: add method that is getter-setter

--- src/getter-and-setter.ts
+++ src/getter-and-setter.ts
@@ -1,16 +1,5 @@
 export {};
 
-// * owner
-//   * 所有者
-//   * 初期化時に設定できる
-//   * 途中で変更できない
-//   * 参照できる
-// * secretNumber
-//   * 個人番号
-//   * 初期化時に設定できる
-//   * 途中で変更できる
-//   * 参照できない
-
 class MyNumberCard {
   private _owner: string;
   private _secretNumber: number;
@@ -20,26 +9,21 @@ class MyNumberCard {
     this._secretNumber = secretNumber;
   }
 
-  get owner() {
+  get owner() {  // 呼び出しできるようにするメソッド
     return this._owner;
   }
 
-  set secretNumber(secretNumber: number) {
+  set secretNumber(secretNumber: number) { // 変更できるようにするメソッド
     this._secretNumber = secretNumber;
   }
 
   debugPrint() {
-    return `secretNumber: ${this._secretNumber}`;
+    return `secretNumber: ${this._secretNumber}`
   }
 }
 
-let card = new MyNumberCard("ハムさん", 1234567890);
+let card = new MyNumberCard("りゅーじ", 1234567890);
 console.log(card.debugPrint());
 card.secretNumber = 1111111111;
 console.log(card.debugPrint());
-
-// card.owner = "Ham";
-console.log(card.owner);
-// card.secretNumber;
 // card._secretNumber;
-console.log(card.secretNumber);
